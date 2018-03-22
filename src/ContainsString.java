8b2185c540036f65098b2e49c144892a7f3afebc
Spider.Man <2430617763@qq.com>
2018-03-22 22:51:26 +0800

    页面进行了删除一些练习的作业

--- src/ContainsString.java
+++
@@ -1,9 +0,0 @@
-
-public class ContainsString {
-	public static void main(String args[]) {
-		String str="½ñÌìµÄ²ËÆ×ÓĞ£ºÕôÑò¸á£¬ÕôĞÜÕÆ£¬ÕôÂ¹Î²£¬ÉÕ»¨Ñ¼£¬À°Èâ£¬ºº±¤";
-		System.out.println(str);
-		boolean request1=str.contains("À°Èâ");
-		System.out.print("ÓĞÃ»ÓĞÀ°Èâ"+request1);
-	}
-}
